b235f34f0846b6ade4c4e68afe6ceab9494d1797
Davide Pollicino <davidepollicino2015@gmail.com>
2020-06-26 20:13:16 +0100

    Adds binary array sorting solution

---
+++ c++/binary-array-sorting.cpp
@@ -0,0 +1,6 @@
+// https://practice.geeksforgeeks.org/problems/binary-array-sorting5355/1/?track=amazon-arrays&batchId=192
+vector<int> SortBinaryArray(vector<int> binArray)
+{
+    sort(binArray.begin(), binArray.end());
+    return binArray;
+}
